17a20cf77ac4569b5edaef40d964104d92078be0
becover <sedi.september@gmail.com>
2021-01-04 22:05:31 +0900

    basic setting commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+import "./App.css";
+
+function App() {
+  return <h1>hello world!</h1>;
+}
+
+export default hot(App);
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const config = {
+  entry: {
+    app: ["react-hot-loader/patch", "./src/index.js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
+  },
+  devServer: {
+    contentBase: "./dist",
+    index: "index.html",
+    port: 9000,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public/index.html" }],
+    }),
+    new HtmlWebpackPlugin({
+      appMountId: "app",
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.hot) {
+    // Cannot use 'contenthash' when hot reloading is enabled.
+    config.output.filename = "[name].[hash].js";
+  }
+
+  return config;
+};
